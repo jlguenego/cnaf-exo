417febdb79066e60e7b1a01709bb1df3c77c6bf1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-03-06 16:41:16 +0100

    add: index.html

---
+++ app/app.js
@@ -0,0 +1,30 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('main', []);
+
+	app.directive('cafHeader', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/caf-header.html'
+		};
+	});
+	
+	
+	app.directive('cafBody', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/caf-body.html'
+		};
+	});
+	
+	
+	app.directive('cafFooter', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/caf-footer.html'
+		};
+	});
+	
+	
+})();
