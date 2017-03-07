00652931fcc66a675cc3661b309e68d9d03575a6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-03-07 16:29:19 +0100

    add: html5

--- app/cnaf-route/cnaf-route.js
+++ app/cnaf-route/cnaf-route.js
@@ -1,35 +1,37 @@
-(function() {
+(function () {
 	'use strict';
 
 	var app = angular.module('cnaf-route', ['ui.router']);
 
-	app.config(['$stateProvider', function($stateProvider) {
+	app.config(['$locationProvider', '$stateProvider', function ($locationProvider, $stateProvider) {
+		$locationProvider
+			.html5Mode(true);
+
 
 		var accueilState = {
 			name: 'accueil',
 			url: '/',
 			templateUrl: 'cnaf-route/tmpl/accueil.html'
 		};
-		
+
 		var produitState = {
 			name: 'produit',
 			url: '/produit',
 			templateUrl: 'cnaf-route/tmpl/produit.html'
 		};
-		
+
 		var serviceState = {
 			name: 'service',
 			url: '/service',
 			templateUrl: 'cnaf-route/tmpl/service.html'
 		};
-		
+
 		var contactState = {
 			name: 'contact',
 			url: '/contact',
 			templateUrl: 'cnaf-route/tmpl/contact.html'
 		};
 
-		
 
 		$stateProvider.state(accueilState);
 		$stateProvider.state(produitState);
